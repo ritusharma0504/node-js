87b835c9c814c6180fb2ea68392e7119f308dc16
ritusharma0504 <ritusharma7924@gmail.com>
2024-07-25 17:16:13 +0530

    node js

--- index.js
+++ index.js
@@ -17,30 +17,39 @@
 // console.log(num3)
 
 
+var num4 = 1000.55
+console.log(num4)
 
+let num5 = 2000.89
+console.log(num5)
 
-
-
-
-
-
-
+const num6 = 3000.90
+console.log(num6)
 
 let name = "hello"
+console.log(typeof name)
 console.log(name)
 
+let name1 = 'ritu'
+console.log(typeof name1)
+console.log(name1)
 
-
-
-
-
-
-
-
+let char1 = 'R'
+console.log(typeof char1)
+console.log(char1)
 
 let num1 = 1000
 console.log(typeof num1)
+console.log(num1)
 
-let num2 = 2000.33
+let num2 = 2000.66
 console.log(typeof num2)
 console.log(num2)
+
+let bool = true
+console.log(typeof bool)
+console.log(bool)
+
+let bool1 = false
+console.log(typeof bool1)
+console.log(bool1)
