97b16a87f2ee8ef6e5dc5f22d0bef2147bf0e636
ritusharma0504 <ritusharma7924@gmail.com>
2024-07-25 16:34:42 +0530

    first commit

---
+++ 02class/index.js
@@ -0,0 +1,54 @@
+// console.log("ritu")
+// var name = "hello"
+// name = "world"
+// console.log(name)
+
+// let name1 = "hello1"
+// name1 = "world1"
+// console.log(name1)
+
+// const name2 = "hello2"
+// name2 = "world2"
+// console.log(name2)
+
+// var name1 = "hello1"
+// var name1 = "world1"
+// console.log(name1)
+
+// let name2 = "hello1"
+// let name2 = "world"
+// console.log(name2)
+
+// let name3 = "hello3"
+// let name = "world"
+// console.log(name3)
+
+// var name1 = "hello3"
+// var name1 = "world3"
+// console.log(name3)
+
+// const obj = {"name":"ritu","age":19,"islive":true}
+// console.log(obj)
+// console.log(obj["name"])
+// console.log(obj["name"])
+// console.log(obj["age"])
+// console.log(obj["islive"])
+
+// const obj1 = {"first name":"ritu","last name":"sharma","address":"alwar"}
+// obj1["address"]="jaipur"
+// obj1["village"] ="duhar chogan"
+// console.log(obj1)
+
+// const arr = ["ritu",19,"alwar",true]
+// console.log(arr)
+
+// const arr = ["ritu", 19, "alwar", true"]
+//     arr["0"] = "hii"
+//     console.log(arr)
+//     console.log(arr[0])
+//     console.log(arr[1])
+
+
+let num1 = {"age":2,"age1":3}
+let num2 = {"age":24,"age1":26}
+console.log(num1["age"]+num2["age1"])
---
+++ index.js
@@ -0,0 +1,46 @@
+// var name="welcome"
+// console.log("name");
+// let name1 = "i am strong"
+// console.log(name1)
+
+// const name2 ="hello,i am ritu"
+// console.log(name2)
+
+
+// var num1 = 1000
+// console.log(num1)
+
+// let num2 = 2000
+// console.log(num2)
+
+// const num3 = 3000
+// console.log(num3)
+
+
+
+
+
+
+
+
+
+
+
+let name = "hello"
+console.log(name)
+
+
+
+
+
+
+
+
+
+
+let num1 = 1000
+console.log(typeof num1)
+
+let num2 = 2000.33
+console.log(typeof num2)
+console.log(num2)
