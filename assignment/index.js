d0db4b049fe3f7b97f87344641acc803000cda56
ritusharma0504 <ritusharma7924@gmail.com>
2024-07-26 13:20:19 +0530

    assignment

---
+++ assignment/index.js
@@ -0,0 +1,22 @@
+ var num1=[40,60,90]
+ var num2=[66,77,99]
+ console.log(num1[0]+num2[1])
+ console.log(num1[2]-num2[1])
+ console.log(num1[2]*num2[1])
+ console.log(num1[0]/num2[2])
+ console.log(num1[2]%num2[1])
+
+ let num3=40
+ let num4=60
+ console.log(num3+num4)
+ console.log(num4-num3)
+ console.log(num3*num4)
+ console.log(num3/num4)
+
+const num5={"age":20,"age1":24}
+const num6={"age":26,"age1":34}
+console.log(num5["age"]+num6["age1"])
+console.log(num5["age1"]-num6["age"])
+console.log(num6["age"]*num5["age1"])
+console.log(num6["age1"]/num5["age"])
+console.log(num5["age1"]%num6["age"])
