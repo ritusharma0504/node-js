087b5b427b9d683b674ef1580391a821d8f7fe4a
ritusharma0504 <ritusharma7924@gmail.com>
2024-07-27 15:22:52 +0530

    number

---
+++ 04class/index.js
@@ -0,0 +1,52 @@
+//let num1=10;
+//let num2=20;
+
+//console.log(num2>num1)
+//console.log(num1>num2)
+
+
+//let num1=10
+//let num2=10
+
+//console.log(num1==num2)
+
+//let num1=10
+//let num2="10"
+
+//console.log(num1);
+//console.log(num2);
+
+//console.log(num1==num2);
+//console.log(num1===num2);
+
+
+//console.log(typeof num1)
+//console.log(typeof num2)
+
+let num1=18
+let num2=18
+
+//num1+=2
+
+//console.log(num1)
+//console.log(num2)
+
+//num1-=2
+
+//console.log(num1)
+//console.log(num2)
+
+//num1*=2
+
+//console.log(num1)
+//console.log(num2)
+
+//num1/=2
+
+//console.log(num1)
+//console.log(num2)
+
+num1**=2
+
+console.log(num1)
+console.log(num2)
