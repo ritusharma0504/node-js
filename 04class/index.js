71741946c29d7a2c68ea1b13d22eaca6f3eaf8b8
ritusharma0504 <ritusharma7924@gmail.com>
2024-07-27 16:23:01 +0530

    number

--- 04class/index.js
+++ 04class/index.js
@@ -23,8 +23,8 @@
 //console.log(typeof num1)
 //console.log(typeof num2)
 
-let num1=18
-let num2=18
+//let num1=18
+//let num2=18
 
 //num1+=2
 
@@ -46,7 +46,34 @@ let num2=18
 //console.log(num1)
 //console.log(num2)
 
-num1**=2
+//num1**=2
 
-console.log(num1)
-console.log(num2)
+//console.log(num1)
+//console.log(num2)
+
+//let num1=10
+//let num2=20
+//let num3=30
+//
+//console.log(num1>num2 && num1>num3)
+//console.log(num2>num1 && num2>num3)
+//console.log(num3>num2 && num3>num1)
+//console.log(num3>num3 && num3<num1)
+//
+//console.log(num1<num2||num1<num3)
+//console.log(num1>num2||num1<num3)
+//console.log(num1<num2||num1>num3)
+//console.log(num1>num2||num1>num3)
+
+//let num1=20
+//let num2=30
+//let result=(num1>num2) ? "yes" :"no"
+//
+//console.log(result)
+
+let num1=40
+let num2=200
+let num3=30
+
+let result=(num1>num2)?((num1>num3)?"num1":"num3"):((num2>num3)?"num2":"num3")
+console.log(result)
